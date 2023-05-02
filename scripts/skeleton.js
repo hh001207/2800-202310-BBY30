1db1cdee79e0e2dc645806800980fdfe6d6a529d
Jeen Namkung <jnamkung@my.bcit.ca>
2023-05-02 13:03:26 -0700

    evert "set up template, navbar, and footer with just the basic structure without any content."
    This reverts commit fac48f33ea6a0772b2f31419ca65f39869418cbf.

--- scripts/skeleton.js
+++
@@ -1,7 +0,0 @@
-// This if function to load skeletons
-// (navbar, footer, and other things) into html.
-function loadSkeleton() {
-	$('#navbarPlaceholder').load('./text/navbar.html');
-	$('#footerPlaceholder').load('./text/footer.html');
-}
-loadSkeleton();
