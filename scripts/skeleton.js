a9c98dc63c28c00934ddab1cbdf64b3aa28b75c6
Jeen Namkung <jnamkung@my.bcit.ca>
2023-05-09 10:18:51 -0700

    changed navbar, footer, map, setting, template.html files to .ejs

--- scripts/skeleton.js
+++
@@ -1,6 +0,0 @@
-//Function to load skeleton into html.
-function loadSkeleton() {
-	$('#navbarPlaceholder').load('./text/navbar.html');
-	$('#footerPlaceholder').load('./text/footer.html');
-}
-loadSkeleton();
