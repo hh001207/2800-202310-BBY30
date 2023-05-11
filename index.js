83fe76f762607fe06bc9dcf5f2a3c8663e80b12f
Ning Feng <nfeng2@my.bcit.ca>
2023-05-11 16:18:21 -0700

    modify index.js for setup mongodb's host

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 
 const app = express();
-
+require('dotenv').config();
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+const Joi = require('joi');
 const port = process.env.PORT || 3030;
+const expireTime = 24 * 60 * 60 * 1000;
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+var {database} = include('./scripts/databaseConnection');
 
 app.set("view engine", "ejs");
 
