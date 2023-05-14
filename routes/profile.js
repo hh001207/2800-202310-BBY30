8d13a1843db1e45cd0864b2bf8aaa01cdc2d9172
ys421 <ychung47@my.bcit.ca>
2023-05-13 17:43:56 -0700

    profile function edited

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongodb_database = process.env.MONGODB_DATABASE;
+const { database } = require('../databaseConnection');
+const userCollection = database.db(mongodb_database).collection('users');
+router.use(express.urlencoded({ extended: false }));
+const ObjectId = require("mongodb").ObjectId;
 
+// GET route to render the profile page
 router.get('/profile', (req, res) => {
   res.render('profile.ejs', {
     username: req.session.username,
@@ -11,5 +17,42 @@ router.get('/profile', (req, res) => {
   });
 });
 
-module.exports = router;
+// router.post('/profile', async (req, res) => {
+//   const {
+//     username,
+//     firstname,
+//     lastname,
+//     gender,
+//     age,
+//     job,
+//     email,
+//     password,
+//     country,
+//     city
+//   } = req.body;
+//   try {
+//     await userCollection.updateOne(
+//       { _id: ObjectId(req.session.userId) },
+//       {
+//         $set: {
+//           username,
+//           firstname,
+//           lastname,
+//           gender,
+//           age,
+//           job,
+//           email,
+//           password,
+//           country,
+//           city
+//         }
+//       }
+//     );
+//     res.status(200).send("Profile updated successfully.");
+//   } catch (error) {
+//     console.error("Error updating user profile:", error);
+//     res.status(500).send("Error updating profile.");
+//   }
+// });
 
+module.exports = router;
