124154a08259e2b129d6c95d00337d55a8d5b5c8
Ning Feng <nfeng2@my.bcit.ca>
2023-05-15 10:36:41 -0700

    modify the report_succeed.js

--- routes/report_succeed.js
+++ routes/report_succeed.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//fix the requireAuth conflict
 router.get('/report_succeed', (req, res) => {
   res.render('report_succeed.ejs');
 });
