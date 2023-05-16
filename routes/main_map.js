4f79a87229e5cc944e160016df46fc3465b18fb1
hh001207 <heeho3464@gmail.com>
2023-05-16 14:06:02 -0700

    first google maps api test

--- routes/main_map.js
+++ routes/main_map.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/main_map', (req, res) => {
-  res.render('main_map.ejs');
+  // Render the main_map.ejs template and pass the Google Maps API key
+  res.render('main_map.ejs', { apiKey: 'YOUR_GOOGLE_MAPS_API_KEY' });
 });
 
 module.exports = router;
