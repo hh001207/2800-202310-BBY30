01f2e5e9d428af0266f1c319af0cbb7561f70d1c
Jeen Namkung <jnamkung@my.bcit.ca>
2023-05-24 14:24:54 -0700

    organized login/signup/askAI/home/about_contact.js

--- routes/about_contact.js
+++ routes/about_contact.js
@@ -1,9 +1,14 @@
+// Import required libraries and packages
 const express = require('express');
+
+// Create a new router
 const router = express.Router();
 
+// Route to render about_contact form
 router.get('/about_contact', (req, res) => {
-  var isAuthenticated = req.session.authenticated;
-  res.render('about_contact.ejs', {authenticated: isAuthenticated });
+	var isAuthenticated = req.session.authenticated;
+	res.render('about_contact.ejs', { authenticated: isAuthenticated });
 });
 
+// Export the router
 module.exports = router;
--- routes/askAI.js
+++ routes/askAI.js
@@ -1,40 +1,48 @@
+// Import required libraries and packages
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Configuration, OpenAIApi } = require('openai');
-const router = express.Router();
-
 require('dotenv').config();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// Create a new router
+const router = express.Router();
 
+// Initialize OpenAI configuration with API Key from environment variables
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
 const openai = new OpenAIApi(configuration);
+
+// Middleware to parse JSON bodies
 router.use(bodyParser.json());
 
+// Route to render AI question form
 router.get('/ask', (req, res) => {
-  var isAuthenticated = req.session.authenticated;
-  res.render('askAI.ejs', { username: req.session.username, authenticated: isAuthenticated });
+	var isAuthenticated = req.session.authenticated;
+	res.render('askAI.ejs', { username: req.session.username, authenticated: isAuthenticated });
 });
 
+// Route to post a question to AI and get a response
 router.post('/ask', async (req, res) => {
-  const question = req.body.question;
-  const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `Q: ${question}\nA:`,
-      temperature: 0,
-      max_tokens: 500,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-      stop: ['\n'],
-  });
+	const question = req.body.question;
+	const response = await openai.createCompletion({
+		model: 'text-davinci-003',
+		prompt: `Q: ${question}\nA:`,
+		temperature: 0,
+		max_tokens: 1000,
+		top_p: 1,
+		frequency_penalty: 0.0,
+		presence_penalty: 0.0,
+		stop: ['\n'],
+	});
 
-  if (response.data.choices && response.data.choices.length > 0 && response.data.choices[0].text) {
-      res.json({ answer: response.data.choices[0].text.trim() });
-  } else {
-      res.json({ answer: 'No response from the API' });
-  }
+	// Check if the response from OpenAI is valid
+	if (response.data.choices && response.data.choices.length > 0 && response.data.choices[0].text) {
+		res.json({ answer: response.data.choices[0].text.trim() });
+	} else {
+		res.json({ answer: 'No response from the API' });
+	}
 });
 
+// Export the router
 module.exports = router;
